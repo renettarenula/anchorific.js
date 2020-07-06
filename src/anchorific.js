1e6000bf2ad2619f98bc7ca6fbf6ddf2724a5059
Aysha Anggraini <ren.aysha@gmail.com>
2020-07-06 21:46:20 +0800

    Add husky :dog: and prettier :nail_care:

--- src/anchorific.js
+++ src/anchorific.js
@@ -1,7 +1,7 @@
 /*
 	The MIT License (MIT)
 
-	Copyright (c) <2013> <Ren Aysha>
+	Copyright (c) <2020> <Aysha Anggraini>
 
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
@@ -22,7 +22,7 @@
 	THE SOFTWARE.
 */
 
-if (typeof Object.create !== "function") {
+if (typeof Object.create !== 'function') {
   Object.create = function (obj) {
     function F() {}
     F.prototype = obj;
@@ -31,7 +31,7 @@ if (typeof Object.create !== "function") {
 }
 
 (function ($, window, document, undefined) {
-  "use strict";
+  'use strict';
 
   var Anchorific = {
     init: function (options, elem) {
@@ -47,21 +47,21 @@ if (typeof Object.create !== "function") {
 
       // Fix bug #1
       if (self.headers.length !== 0) {
-        self.first = parseInt(self.headers.prop("nodeName").substring(1), null);
+        self.first = parseInt(self.headers.prop('nodeName').substring(1), null);
       }
 
       self.build();
     },
 
     opt: {
-      navigation: ".anchorific", // position of navigation
-      headers: "h1, h2, h3, h4, h5, h6", // custom headers selector
+      navigation: '.anchorific', // position of navigation
+      headers: 'h1, h2, h3, h4, h5, h6', // custom headers selector
       speed: 200, // speed of sliding back to top
-      anchorClass: "anchor", // class of anchor links
-      anchorText: "#", // prepended or appended to anchor headings
-      top: ".top", // back to top button or link class
+      anchorClass: 'anchor', // class of anchor links
+      anchorText: '#', // prepended or appended to anchor headings
+      top: '.top', // back to top button or link class
       spy: true, // scroll spy
-      position: "append", // position of anchor text
+      position: 'append', // position of anchor text
       spyOffset: !0, // specify heading offset for spy scrolling
     },
 
@@ -71,8 +71,8 @@ if (typeof Object.create !== "function") {
         navigations = function () {};
       // when navigation configuration is set
       if (self.opt.navigation) {
-        $(self.opt.navigation).append("<ul />");
-        self.previous = $(self.opt.navigation).find("ul").last();
+        $(self.opt.navigation).append('<ul />');
+        self.previous = $(self.opt.navigation).find('ul').last();
         navigations = function (obj) {
           return self.navigations(obj);
         };
@@ -96,15 +96,15 @@ if (typeof Object.create !== "function") {
         which,
         name = self.name(obj);
 
-      if (obj.attr("id") !== undefined) name = obj.attr("id");
+      if (obj.attr('id') !== undefined) name = obj.attr('id');
 
-      link = $("<a />")
-        .attr("href", "#" + name)
+      link = $('<a />')
+        .attr('href', '#' + name)
         .text(obj.text());
-      list = $("<li />").append(link);
+      list = $('<li />').append(link);
 
-      which = parseInt(obj.prop("nodeName").substring(1), null);
-      list.attr("data-tag", which);
+      which = parseInt(obj.prop('nodeName').substring(1), null);
+      list.attr('data-tag', which);
 
       self.subheadings(which, list);
 
@@ -113,18 +113,18 @@ if (typeof Object.create !== "function") {
 
     subheadings: function (which, a) {
       var self = this,
-        ul = $(self.opt.navigation).find("ul"),
-        li = $(self.opt.navigation).find("li");
+        ul = $(self.opt.navigation).find('ul'),
+        li = $(self.opt.navigation).find('li');
 
       if (which === self.first) {
         self.previous.append(a);
       } else if (which > self.first) {
-        li.last().append("<ul />");
+        li.last().append('<ul />');
         // can't use cache ul; need to find ul once more
-        $(self.opt.navigation).find("ul").last().append(a);
+        $(self.opt.navigation).find('ul').last().append(a);
         self.previous = a.parent();
       } else {
-        $("li[data-tag=" + which + "]")
+        $('li[data-tag=' + which + ']')
           .last()
           .parent()
           .append(a);
@@ -135,8 +135,8 @@ if (typeof Object.create !== "function") {
     name: function (obj) {
       var name = obj
         .text()
-        .replace(/[^\w\s]/gi, "")
-        .replace(/\s+/g, "-")
+        .replace(/[^\w\s]/gi, '')
+        .replace(/\s+/g, '-')
         .toLowerCase();
 
       return name;
@@ -150,16 +150,16 @@ if (typeof Object.create !== "function") {
         klass = self.opt.anchorClass,
         id;
 
-      if (obj.attr("id") === undefined) obj.attr("id", name);
+      if (obj.attr('id') === undefined) obj.attr('id', name);
 
-      id = obj.attr("id");
+      id = obj.attr('id');
 
-      anchor = $("<a />")
-        .attr("href", "#" + id)
+      anchor = $('<a />')
+        .attr('href', '#' + id)
         .html(text)
         .addClass(klass);
 
-      if (self.opt.position === "append") {
+      if (self.opt.position === 'append') {
         obj.append(anchor);
       } else {
         obj.prepend(anchor);
@@ -168,10 +168,10 @@ if (typeof Object.create !== "function") {
 
     back: function () {
       var self = this,
-        body = $("body, html"),
+        body = $('body, html'),
         top = $(self.opt.top);
 
-      top.on("click", function (e) {
+      top.on('click', function (e) {
         e.preventDefault();
 
         body.animate(
@@ -218,13 +218,13 @@ if (typeof Object.create !== "function") {
 
         if (current && current.length) {
           // get all li tag that contains href of # ( all the parents )
-          list = $('li:has(a[href="#' + current.attr("id") + '"])');
+          list = $('li:has(a[href="#' + current.attr('id') + '"])');
 
           if (prev !== undefined) {
-            prev.removeClass("active");
+            prev.removeClass('active');
           }
 
-          list.addClass("active");
+          list.addClass('active');
           prev = list;
         }
       });
@@ -233,12 +233,12 @@ if (typeof Object.create !== "function") {
 
   $.fn.anchorific = function (options) {
     return this.each(function () {
-      if (!$.data(this, "anchorific")) {
+      if (!$.data(this, 'anchorific')) {
         var anchor = Object.create(Anchorific);
 
         anchor.init(options, this);
 
-        $.data(this, "anchorific", anchor);
+        $.data(this, 'anchorific', anchor);
       }
     });
   };
